f0fd0b3b9a93945d137188c11641e59ab6f04cb4
Nirmal Kumar Das <you@example.com>
2024-12-11 19:12:43 +0530

    First Commit

---
+++ server/src/models/favorite.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import modelOptions from "./model.options.js";
+
+const { Schema } = mongoose;
+
+const favoriteSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    mediaType: {
+      type: String,
+      enum: ["tv", "movie"],
+      required: true,
+    },
+    mediaId: {
+      type: String,
+      required: true,
+    },
+    mediaTitle: {
+      type: String,
+      required: true,
+    },
+    mediaPoster: {
+      type: String,
+      required: true,
+    },
+    mediaRate: {
+      type: Number,
+      required: true,
+    },
+  },
+  modelOptions
+);
+
+export default mongoose.model("Favorite", favoriteSchema);
+
